936f12e24450442e12c7139d5828fdd3a27a98e8
PJ Bryan <pjbryan2004@gmail.com>
2026-09-09 10:01:25 -0400

    added comments  and character profile

--- character_profile.py
+++ character_profile.py
@@ -1,1 +1,14 @@
-print ("Hello world")
+# PJ Bryan
+# CMP-131-80230
+# Week 3
+# Lab 01
+# Assignment 2
+# 9/9/2026
+# * Important accomplishment
+# * Interesting fact
+
+
+name = "Ruijerd Superdia"
+age = 1000
+occupational_Role = "Tribe Ambassador"
+print(age)
