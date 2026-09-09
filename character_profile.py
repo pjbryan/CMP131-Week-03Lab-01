ba6db466b43819dfef57d63e84b872feae3f0444
PJ Bryan <pjbryan2004@gmail.com>
2026-09-09 10:16:02 -0400

    finished part 2

--- character_profile.py
+++ character_profile.py
@@ -4,14 +4,17 @@
 # Lab 01
 # Assignment 2
 # 9/9/2026
-# * Important accomplishment
-# * Interesting fact
-
+# Strength, Weakness, residence
 
 name = "Ruijerd Superdia"
 age = 1000
 occupational_Role = "Tribe Ambassador"
 important_Accomplishment = "Restored honor to the Superd tribe"
-interesting_Fact = 
+companion = "Norn Greyrat"
+residence = "Greyrat family residence in Sharia, the Magic City"
+strength = "Third Eye: Acts as a radar, able to locate enemies which are not in sight."
+
+
+
 
 print()
