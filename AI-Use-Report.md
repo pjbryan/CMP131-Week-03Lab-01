276698a08b5ed9568fac65689977f530ebaa1137
PJ Bryan <pjbryan2004@gmail.com>
2026-09-09 20:45:23 -0400

    Assignment 2 completed

--- AI-Use-Report.md
+++ AI-Use-Report.md
@@ -11,8 +11,8 @@
 
 Did you use an AI tool for this lab?
 
-- [x] Yes
-- [ ] No
+- [ ] Yes
+- [x] No
 
 If yes, complete the sections below. If no, write “No AI tool was used” under Summary.
 
@@ -26,19 +26,19 @@ Tool: ChatGPT
 
 Describe what you asked the AI tool to help you understand or troubleshoot.
 
-Response: where does Ruijerd live after marrying Norn Greyrat?
+Response:
 
 ## How I Used the Assistance
 
 Explain which suggestions you used, changed, or rejected. Do not paste an entire AI conversation.
 
-Response: After Ruijerd and Norn marry, they live together in Sharia, the Magic City. Ruijerd becomes integrated into Norn's life there rather than returning to wandering as a Superd warrior.
+Response:
 
 ## Testing and Verification
 
 Explain how you tested the code and confirmed that the AI assistance was accurate.
 
-Response: AI mixed up the characters and thought that it was Rudeus that marries Norn. They did not. They are siblings.
+Response:
 
 ## What I Learned
 
--- character_profile.py
+++ character_profile.py
@@ -8,20 +8,23 @@
 name = "Ruijerd Superdia"
 age = 566
 occupationalRole = "Tribe Ambassador, Adventurer"
-importantAccomplishment = "Restored honor to the Superd tribe"
+importantAccomplishment = "Restored honor to the Superd tribe."
 companion = "Norn Greyrat"
-residence = "Greyrat family residence in Sharia, the Magic City"
+residence = "Greyrat family residence in Sharia, the Magic City."
 strength = "Third Eye: Acts as a radar, able to locate enemies which are not in sight."
+divider = ("____________________________________________________________________________________________________________________________")
 
+format = "Character Description " + divider
+print(format)
 print("Name:",name)
 print("Age: approximately",age,"years old.")
 print("Occupation:",occupationalRole)
 print("Accomplishment:",importantAccomplishment)
-print("<3:",companion)
+print("Companion <3:",companion)
 print("Residence:",residence)
 print("Strengths:",strength)
 
-print("-------------------------------")
- 
+format = "Quote " + divider
+print(format)
 favoriteQuote = 'Ruijerd: "Do you want us to go extinct?! Rudeus brought that man here, and I trust Rudeus. If you have complaints, save them until after I’m dead."'
 print(favoriteQuote)
